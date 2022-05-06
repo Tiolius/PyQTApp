01b3e10c0cd6131577d24e741c74a3158bede6d5
Stanislav <morel7@mail.ru>
2022-05-06 19:01:08 +0300

    0.1

--- PyQTApp.py
+++ PyQTApp.py
@@ -7,10 +7,24 @@ import sys
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        self.windowTitleChanged.connect(self.onWindowTitleChange)
+        self.windowTitleChanged.connect(lambda x: self.my_custom_fn())
+        self.windowTitleChanged.connect(lambda x: self.my_custom_fn(x))
+        self.windowTitleChanged.connect(lambda x: self.my_custom_fn(x, 25))
         self.setWindowTitle("PyQT5 App")
-        label = QLabel('some Label')
+
+        label = QLabel("Usual Label")
         label.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(label)
+
+    def onWindowTitleChange(self, s):
+        print(s)
+
+    def my_custom_fn(self, a="Привет", b=5):
+        print(a, b)
+
+    def contextMenuEvent(self, event):
+        print("Contex menu event!")
 
 
 app = QApplication(sys.argv)
